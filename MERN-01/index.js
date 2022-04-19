f8222e805db29bf62a1f6352a68ba65b568c01ce
Sumaira Mehdi <sumairanmehdi@gmail.com>
2022-04-19 16:07:22 +0500

    more files added

---
+++ MERN-01/index.js
@@ -0,0 +1,11 @@
+// MERN ASSIGNMENT NO 1
+// Print carnames in an array using loop.
+
+function carNames(){
+    let car = ["Honda","Toyota","Hybrid","Audi","Suzuki"];
+        for( let i = 0;i < car.length;i++){
+        console.log(car[i]);
+    }
+}
+carNames();
+
