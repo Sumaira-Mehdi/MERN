138f26730f2025c2914e89a413ce814ea3e24a52
Aamna-Arshad <104038814+Aamna-Arshad@users.noreply.github.com>
2022-04-20 08:58:39 +0500

    Add files via upload

---
+++ amna-assign1.js
@@ -0,0 +1,16 @@
+// Assignment 1
+// creat an array of a Car name and print single car name 
+
+const cars = new Array("Ferari", "Porche", "Mercedese", "Land Cruiser");
+
+cars.forEach(value => console.log(value));
+
+console.log(cars);
+
+
+
+/* class Task
+let yob = 1985;
+let age = 2022- yob;
+
+ console.log("your age is : ", age); */
