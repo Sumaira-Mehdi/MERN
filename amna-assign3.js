ad55d9fd7ae801e30e371b2d0c023f46cdcfe010
Aamna-Arshad <104038814+Aamna-Arshad@users.noreply.github.com>
2022-04-20 09:10:59 +0500

    Assignment 3 added to repository

---
+++ amna-assign3.js
@@ -0,0 +1,18 @@
+// Assignment 3
+// Write a function that takes in 2 parameters, 1 string and 1 number and print the string the number 
+// of times mentioned in the Number param.
+
+let myFunc = (str, numOfRotation) => {
+    
+    for(let i = 0; i < numOfRotation; i++){
+        console.log(str);
+    }
+}
+    
+myFunc("JavaScript", 5);
+
+
+
+
+
+
