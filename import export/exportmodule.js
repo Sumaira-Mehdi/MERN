212a497dcb9282ae9b8180da8093f6ff679f8cd4
Sumaira Mehdi <sumairanmehdi@gmail.com>
2022-04-20 07:34:19 +0500

    Added the import export folder

---
+++ import export/exportmodule.js
@@ -0,0 +1,8 @@
+//Exporting 
+let celciusToFarenhiet = (celcius) => {
+    let farenhiet = celcius * 9/5 + 32;
+    console.log(farenhiet);
+
+}
+//celciusToFarenhiet(20)
+export{celciusToFarenhiet}
---
+++ import export/importmodule.js
@@ -0,0 +1,4 @@
+//Importing
+import {celciusToFarenhiet} from "./exportmodule.js";
+console.log(celciusToFarenhiet(20));
+
