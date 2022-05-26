4e084631dd3bd5e6c187acde497b6e716a94d09c
Sumaira Mehdi <sumairanmehdi@gmail.com>
2022-05-27 02:08:01 +0500

    exchange rate fetching API folder modified and added async awaiot code

--- exchange_rate_fetch/index.js
+++ exchange_rate_fetch/index.js
@@ -3,11 +3,13 @@ import fetch from 'node-fetch';
 const app = express();
 const PORT = 8080;
 
+//creating server.
 app.get('/conversion/:number', (req,res) => {
+    //exchange rate API.
     const myUrl = "https://v6.exchangerate-api.com/v6/15fba9b3458d5ac9c966b0e1/latest/USD";
      const dollar = req.params.number;
-     console.log(dollar);
-
+     
+    //Fetching Api.
      const apiUrl = fetch(myUrl)
      .then(response => response.json())
      .then(data => {
@@ -17,4 +19,7 @@ app.get('/conversion/:number', (req,res) => {
      })
 
 })
+
 app.listen(PORT);
+
+
